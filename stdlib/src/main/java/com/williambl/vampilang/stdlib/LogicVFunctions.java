8881ae163022ab2a5024d3db99b31cc2ac2fcdb5
williambl <william@williambl.com>
2023-08-03 13:41:43 +0100

    feat: add logic v-functions

---
+++ stdlib/src/main/java/com/williambl/vampilang/stdlib/LogicVFunctions.java
@@ -0,0 +1,41 @@
+package com.williambl.vampilang.stdlib;
+
+import com.williambl.vampilang.lang.VValue;
+import com.williambl.vampilang.lang.function.VFunctionDefinition;
+import com.williambl.vampilang.lang.function.VFunctionSignature;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public final class LogicVFunctions {
+    public static final VFunctionDefinition AND = fromMultiOperator("and", bools -> bools.allMatch(Boolean::booleanValue));
+    public static final VFunctionDefinition OR = fromMultiOperator("or", bools -> bools.anyMatch(Boolean::booleanValue));
+    public static final VFunctionDefinition NOT = fromUnaryOperator("not", bool -> !bool);
+
+    public static VFunctionDefinition fromBinaryOperator(String name, BiFunction<Boolean, Boolean, Boolean> operator) {
+        return new VFunctionDefinition(
+                name,
+                new VFunctionSignature(Map.of("a", StandardVTypes.BOOLEAN, "b", StandardVTypes.BOOLEAN), StandardVTypes.BOOLEAN),
+                (ctx, sig, args) -> new VValue(sig.outputType(), operator.apply((Boolean) args.get("a").value(), (Boolean) args.get("b").value())));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static VFunctionDefinition fromMultiOperator(String name, Function<Stream<Boolean>, Boolean> operator) {
+        return new VFunctionDefinition(
+                name,
+                new VFunctionSignature(Map.of("operands", StandardVTypes.LIST.with(0, StandardVTypes.BOOLEAN)), StandardVTypes.BOOLEAN),
+                (ctx, sig, args) -> new VValue(sig.outputType(), operator.apply(((List<Boolean>) args.get("operands").value()).stream())));
+    }
+
+    public static VFunctionDefinition fromUnaryOperator(String name, Function<Boolean, Boolean> operator) {
+        return new VFunctionDefinition(
+                name,
+                new VFunctionSignature(Map.of("operand", StandardVTypes.BOOLEAN), StandardVTypes.BOOLEAN),
+                (ctx, sig, args) -> new VValue(sig.outputType(), operator.apply((Boolean) args.get("operand").value())));
+    }
+}
