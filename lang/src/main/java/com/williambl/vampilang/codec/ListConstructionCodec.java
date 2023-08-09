8c1274e69926c3aefc239483d1364f6520a63859
williambl <william@williambl.com>
2023-08-09 18:26:51 +0100

    fix: don't always try and use every codec in the list codec

--- lang/src/main/java/com/williambl/vampilang/codec/ListConstructionCodec.java
+++ lang/src/main/java/com/williambl/vampilang/codec/ListConstructionCodec.java
@@ -7,32 +7,39 @@ import com.mojang.serialization.DynamicOps;
 import com.williambl.vampilang.lang.EvaluationContext;
 import com.williambl.vampilang.lang.VEnvironment;
 import com.williambl.vampilang.lang.VExpression;
-import com.williambl.vampilang.lang.type.ConstructableVType;
+import com.williambl.vampilang.lang.type.VParameterisedType;
 import com.williambl.vampilang.lang.type.VType;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ListConstructionCodec implements Codec<VExpression.ListConstruction> {
     private final VEnvironment vTypeCodecs;
     private final EvaluationContext.Spec spec;
+    private final VParameterisedType expectedType;
 
-    public ListConstructionCodec(VEnvironment vTypeCodecs, EvaluationContext.Spec spec) {
+    public ListConstructionCodec(VEnvironment vTypeCodecs, EvaluationContext.Spec spec, VType expectedType) {
         this.vTypeCodecs = vTypeCodecs;
         this.spec = spec;
+        this.expectedType = expectedType instanceof VParameterisedType paramed ? paramed : null;
     }
 
     @Override
     public <T> DataResult<Pair<VExpression.ListConstruction, T>> decode(DynamicOps<T> ops, T input) {
-        var listCodec = this.vTypeCodecs.expressionCodecForType(VType.createTemplate(), this.spec).listOf() //TODO cache
-                .xmap(l -> (VExpression.ListConstruction) VExpression.list(l), VExpression.ListConstruction::entries);
-        return listCodec.decode(ops, input);
+        if (this.expectedType != null) {
+            var listCodec = this.vTypeCodecs.expressionCodecForType(this.expectedType.parameters.get(0), this.spec).listOf() //TODO cache
+                    .xmap(l -> (VExpression.ListConstruction) VExpression.list(l), VExpression.ListConstruction::entries);
+            return listCodec.decode(ops, input);
+        } else {
+            return DataResult.error(() -> "No types match");
+        }
     }
 
     @Override
     public <T> DataResult<T> encode(VExpression.ListConstruction input, DynamicOps<T> ops, T prefix) {
-        var listCodec = this.vTypeCodecs.expressionCodecForType(VType.createTemplate(), this.spec).listOf()
-                .xmap(l -> (VExpression.ListConstruction) VExpression.list(l), VExpression.ListConstruction::entries);
-        return listCodec.encode(input, ops, prefix);
+        if (this.expectedType != null) {
+            var listCodec = this.vTypeCodecs.expressionCodecForType(this.expectedType.parameters.get(0), this.spec).listOf()
+                    .xmap(l -> (VExpression.ListConstruction) VExpression.list(l), VExpression.ListConstruction::entries);
+            return listCodec.encode(input, ops, prefix);
+        } else {
+            return DataResult.error(() -> "No types match");
+        }
     }
 }
--- lang/src/main/java/com/williambl/vampilang/lang/VEnvironmentImpl.java
+++ lang/src/main/java/com/williambl/vampilang/lang/VEnvironmentImpl.java
@@ -78,7 +78,7 @@ public class VEnvironmentImpl implements VEnvironment {
                                 Function.identity()),
                 VariableRefCodec.CODEC,
                 new ObjectConstructionCodec(this, spec).comapFlatMap(o -> type.contains(o.resolveTypes(this, spec).type()) ? DataResult.success(o) : DataResult.error(() -> "Unmatched type"), Function.identity()),
-                new ListConstructionCodec(this, spec)));
+                new ListConstructionCodec(this, spec, type)));
     }
 
     @Override
